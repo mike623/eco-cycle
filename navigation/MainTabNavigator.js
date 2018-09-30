3c992e20fa935179b25a91ffde90e126ffb7e5d4
mike_wong <namike623@gmail.com>
2018-10-01 02:02:33 +0800

    update icon

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -4,7 +4,6 @@ import { createStackNavigator, createBottomTabNavigator } from 'react-navigation
 
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
 import SelectionScreen from '../screens/SelectionScreen';
 import SearchPage from '../screens/SearchPage';
 import MapScreen from '../screens/MapScreen';
@@ -26,20 +25,6 @@ HomeStack.navigationOptions = {
     />
   ),
 };
-
-// const LinksStack = createStackNavigator({
-//   Links: LinksScreen,
-// });
-
-// LinksStack.navigationOptions = {
-//   tabBarLabel: 'Links',
-//   tabBarIcon: ({ focused }) => (
-//     <TabBarIcon
-//       focused={focused}
-//       name={'md-map'}
-//     />
-//   ),
-// };
 
 const SettingsStack = createStackNavigator({
   Settings: SettingsScreen,
--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,14 +1,45 @@
-import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import React from "react";
+import { View, TouchableOpacity } from "react-native";
+import { MonoText } from "../components/StyledText";
+
+const Item = ({ children, onPress }) => (
+  <TouchableOpacity onPress={onPress}>
+    <View backgroundColor='white' paddingVertical={16} paddingHorizontal={32}>
+      <MonoText>{children}</MonoText>
+    </View>
+  </TouchableOpacity>
+);
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
-    title: 'app.json',
+    title: "Settings"
   };
+
+  _check =  async () => {
+    try {
+      const update = await Expo.Updates.checkForUpdateAsync();
+      if (update.isAvailable) {
+        await Expo.Updates.fetchUpdateAsync();
+        // ... notify user of update ...
+        Expo.Updates.reloadFromCache();
+      } else {
+        alert('update to date!')
+      }
+    } catch (e) {
+      alert(e.message)
+      // handle or log error
+    }
+  }
 
   render() {
     /* Go ahead and delete ExpoConfigView and replace it with your
      * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    return (
+      <View flex={1} backgroundColor='white'>
+        <Item onPress={this._check}>check update</Item>
+        <View marginLeft={32} width='100%' height={1} backgroundColor='#D9D9D9'/>
+        <Item>by: Mike Wong</Item>
+      </View>
+    );
   }
 }
